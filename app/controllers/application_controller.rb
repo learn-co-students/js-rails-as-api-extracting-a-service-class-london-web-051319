5aec36201e50de031dd90afd996aa95d92963014
James <hayward.jp@gmail.com>
2019-06-28 11:08:44 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+   config.middleware.insert_before 0, Rack::Cors do
+      allow do
+          origins '*'
+          resource '*', headers: :any, methods: [:get, :post]
+      end
+    end
+
 end
